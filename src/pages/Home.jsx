d61dc01a374504d256592718321368a972d71895
stephaniepazos89 <stephaniepazos89@gmail.com>
2022-11-04 18:39:18 -0300

    se crea el componente header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import Header from "../Componentes/Header"
-import ItemContenido from "../Componentes/ItemContenido"
+//import ItemContenido from "../Componentes/ItemContenido"
 
 export default function Home() {
     return (
         <>
         <Header></Header>
-        <ItemContenido></ItemContenido>
+        {/*<ItemContenido></ItemContenido>*/}
       </>
     )
 }
