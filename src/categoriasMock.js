67475a0932e6107389465521012e8aa048ad39e8
stephaniepazos89 <stephaniepazos89@gmail.com>
2022-11-13 17:50:25 -0300

    se borran comentarios en contenidos mockeados

--- src/categoriasMock.js
+++ src/categoriasMock.js
@@ -3,21 +3,6 @@ const categorias = [
     'Audio',
     'Video',
     'Documentos',
-    //'Música',
-    //'Deportes',
-    //'Arte',
-    //'Ciencia',
-    //'Cocina',
-    //'Divertimento y juegos',
-    //'Psicología',
-    //'Tecnología',
-    //'Mindfullness',
-    //'Meditación',
-    //'Espiritualidad',
-    //'Web',
-    //'Casamiento',
-    //'Infancia',
-    //S'LGTBIQ+'
 ]
 
 export default categorias
